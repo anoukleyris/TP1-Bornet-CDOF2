c55e380c5313fa90d1b9f986276dead70c930311
Joshua Bornet <joshua.bornet@edu.devinci.fr>
2024-01-30 08:53:45 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='TP1-Bornet-CDOF2',
+ version='1.0',
+ author='LambdaFr',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
