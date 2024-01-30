c5b8efbad69fd8f52774cab336c949e3f030c67f
Joshua Bornet <joshua.bornet@edu.devinci.fr>
2024-01-30 09:11:31 +0100

    game of life

--- tp1.py
+++ tp1.py
@@ -1,13 +1,43 @@
-from art import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-def generate_ascii_art(text):
-    ascii_art = text2art(text)
-    return ascii_art
+ON = 255
+OFF = 0
+size = 50
+density = 0.2
 
-def main():
-    text = input("Enter text : ")
-    result = generate_ascii_art(text)
-    print(result)
+def initialize_grid(size, density=0.2):
+    grid = np.random.choice([OFF, ON], size=(size, size), p=[1 - density, density])
+    return grid
 
-if __name__ == "__main__":
-    main()
+def update(frameNum, img, grid, size):
+    newGrid = grid.copy()
+
+    for i in range(size):
+        for j in range(size):
+            total = int((grid[i, (j-1)%size] + grid[i, (j+1)%size] +
+                         grid[(i-1)%size, j] + grid[(i+1)%size, j] +
+                         grid[(i-1)%size, (j-1)%size] + grid[(i-1)%size, (j+1)%size] +
+                         grid[(i+1)%size, (j-1)%size] + grid[(i+1)%size, (j+1)%size])/ON)
+
+            if grid[i, j] == ON:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = OFF
+            else:
+                if total == 3:
+                    newGrid[i, j] = ON
+
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img
+
+# Initialisation de la grille
+grid = initialize_grid(size, density)
+
+# Configuration de l'affichage
+fig, ax = plt.subplots()
+img = ax.imshow(grid, interpolation='nearest', cmap='gray')
+ani = animation.FuncAnimation(fig, update, fargs=(img, grid, size), frames=10, interval=200, save_count=50)
+
+plt.show()
